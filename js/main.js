bce3b93c79f389506ad96e6448b6b92ed2835ea9
RANI22 <ranip2780@gmail.com>
2020-07-24 20:12:13 +0530

    uploaded

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const responsive = {
+    0:{
+        items:1
+    },
+    320: {
+        items:1
+    },
+    560: {
+        items:2
+    },
+    960: {
+        items:3
+    }
+}
+
+
+
+$(document).ready(function() {
+    $nav=$('.nav');
+    $toggleCollapse=$('.toggle-collapse');
+
+    /* click event on toggle menu */
+
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+    })
+
+//owl carousel
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:false,
+    autoplayTimeout:3000,
+    dots:false,
+    nav:true,
+    navText:[ $('.owl-navi .owl-nav-prev'),$('.owl-navi .owl-nav-next')],
+    responsive: responsive
+});
+
+//click to move up
+$('.move-up span').click(function(){
+    $('html,body').animate({
+        scrollTop:0  
+    },1000);
+});
+
+//AOS
+AOS.init();
+
+
+
+});
